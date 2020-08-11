f9db95baf757a042a8d93c61193c009886a4516c
Nerly Ton <nerlyton@gmail.com>
2020-08-11 21:58:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
     song_name = file.split(" - ")[1]
     self.artist = file.split(" - ")[0]
     song = self.new(song_name)
-    @artist = artist_name
+    artist = artist_name
     song
     # binding.pry
   end
