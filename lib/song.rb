14fc253780dd0b7133c8060a561a535574d675ed
Nerly Ton <nerlyton@gmail.com>
2020-08-11 22:13:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,9 +22,9 @@ class Song
   
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
-    artist_name = file.split(" - ")[0]
+    artist = file.split(" - ")[0]
     song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
+    song.artist_name = artist 
     song
     # binding.pry
   end
